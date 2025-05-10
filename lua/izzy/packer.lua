23f14b72d2931d3dc5ff7c074f9dfafa9c63f9f3
Esatollah Ghazi <esatollah.ghazi@tugraz.at>
2025-05-10 13:38:42 +0200

    changes

--- lua/izzy/packer.lua
+++ lua/izzy/packer.lua
@@ -17,28 +17,17 @@ return require('packer').startup(function(use)
 
   use 'ThePrimeagen/harpoon'
 
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v1.x',
-    requires = {
-      -- LSP Support
-      { 'neovim/nvim-lspconfig' },         -- Required
-      { 'williamboman/mason.nvim' },       -- Optional
-      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+  use{ 'neovim/nvim-lspconfig' }         -- Required
 
-      -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },     -- Required
-      { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-      { 'hrsh7th/cmp-buffer' },   -- Optional
-      { 'hrsh7th/cmp-path' },     -- Optional
-      { 'saadparwaiz1/cmp_luasnip' }, -- Optional
-      { 'hrsh7th/cmp-nvim-lua' }, -- Optional
+  use{ 'hrsh7th/nvim-cmp' }     -- Required
+  use{ 'hrsh7th/cmp-nvim-lsp' } -- Required
+  use{ 'hrsh7th/cmp-buffer' }   -- Optional
+  use{ 'hrsh7th/cmp-path' }     -- Optional
+  use{ 'saadparwaiz1/cmp_luasnip' } -- Optional
+  use{ 'hrsh7th/cmp-nvim-lua' } -- Optional
 
-      -- Snippets
-      { 'L3MON4D3/LuaSnip' },         -- Required
-      { 'rafamadriz/friendly-snippets' }, -- Optional
-    }
-  }
+  use{ 'L3MON4D3/LuaSnip' }         -- Required
+  use{ 'rafamadriz/friendly-snippets' }-- Optional
 
   use {
     'nvim-lualine/lualine.nvim',
