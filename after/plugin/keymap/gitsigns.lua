68d276d050c41c8420fd40d63186fdbe40ba872a
Esatollah Ghazi <esatollah.ghazi@tugraz.at>
2025-07-11 17:34:35 +0200

    .

--- after/plugin/keymap/gitsigns.lua
+++ after/plugin/keymap/gitsigns.lua
@@ -5,5 +5,6 @@ local nnoremap = Keymap.nnoremap
 nnoremap("<leader>gp", "<cmd>Gitsigns preview_hunk<CR>")
 nnoremap("<leader>gP", "<cmd>Gitsigns diffthis<CR>")
 nnoremap("<leader>gu", "<cmd>Gitsigns reset_hunk<CR>")
-nnoremap("<leader>gn", "<cmd>Gitsigns next_hunk<CR>")
-nnoremap("<leader>gN", "<cmd>Gitsigns prev_hunk<CR>")
+nnoremap("<leader>gU", "<cmd>Gitsigns reset_buffer<CR>")
+nnoremap("<C-M>", "<cmd>Gitsigns next_hunk<CR>")
+nnoremap("<C-N>", "<cmd>Gitsigns prev_hunk<CR>")
